6e5ee9fd4c848352d18ec3c3399165cfe4c792f8
Gaston Martinez <gastonm3112@gmail.com>
2021-07-21 13:53:20 -0300

    add jsonwebtoken login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,8 +13,8 @@ const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-
     res.json(new Success(await authService.login(email, password)));
+
   } catch (error) {
     next(error);
   }
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,25 +1,43 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userService = require('../services/userService');
 const AppError = require('../errors/appError');
+const config = require('../config');
 
 const login = async (email, password) => {
-  //TODO: 
-  //Verificar si el mail existe
-  const user = await userService.findByEmail(email);
-  if (!user) {
-    throw new AppError('Email / Password are not correct', 400);
-  }
-  //Verificar si el usuario esta activo
-  if (!user.state) {
-    throw new AppError('This user is disabled', 400);
-  }
-  //Verifcar contraseña
-  const validPassword = bcrypt.compareSync(password, user.password);
-  if (!validPassword) {
-    throw new AppError('Email / Password are not correct', 400);
-  }
-  //Generar JWT
+  try {
+    //Verificar si el mail existe
+    const user = await userService.findByEmail(email);
+    if (!user) {
+      throw new AppError('Email / Password are not correct', 400);
+    }
+    //Verificar si el usuario esta activo
+    if (!user.state) {
+      throw new AppError('This user is disabled', 400);
+    }
+    //Verifcar contraseña
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      throw new AppError('Email / Password are not correct', 400);
+    }
+    //Generar JWT
+    const token = _encrypt(user._id);
 
+    return {
+      token,
+      user: user.name,
+      role: user.role
+    }
+
+  } catch (error) {
+    throw error;
+  }
+
+
+}
+
+_encrypt = (uid) => {
+  return jwt.sign({ uid }, config.auth.secret, { expiresIn: config.auth.expires });
 }
 
 
